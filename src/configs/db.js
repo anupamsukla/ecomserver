def58d5fe9a4fcb423e2bc657059723a0848f182
anupamsukla <32832216+anupamsukla@users.noreply.github.com>
2022-08-06 18:11:07 +0530

    ok

--- src/configs/db.js
+++ src/configs/db.js
@@ -5,4 +5,3 @@ const connectMongoDB = () => {
   return mongoose.connect(uri)
 }
 module.exports = connectMongoDB
-//`mongodb+srv://${process.env.MONGO_USER_NAME}:${process.env.MONGO_PASSWORD}@backend.svdlc.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`
--- src/index.js
+++ src/index.js
@@ -1,47 +1,47 @@
-const express = require("express");
+const express = require('express')
 
-const cors = require("cors");
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-app.use(cors());
+app.use(cors())
 
-const { SignUp, SignIn } = require("./controllers/auth.controller");
+const { SignUp, SignIn } = require('./controllers/auth.controller')
 
-const productController = require("./controllers/product.controller");
-const favouriteController = require("./controllers/favourite.controller");
-const cartController = require("./controllers/cart.controller");
-const amountController = require("./controllers/amount.controller");
-const couponController = require("./controllers/coupon.controller");
-const addressController = require("./controllers/address.controller");
-const orderController = require("./controllers/order.controller");
+const productController = require('./controllers/product.controller')
+const favouriteController = require('./controllers/favourite.controller')
+const cartController = require('./controllers/cart.controller')
+const amountController = require('./controllers/amount.controller')
+const couponController = require('./controllers/coupon.controller')
+const addressController = require('./controllers/address.controller')
+const orderController = require('./controllers/order.controller')
 
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   try {
-    res.status(200).send("Welcome E-Store");
+    res.status(200).send('Welcome To ECOM 101')
   } catch (error) {
-    res.status(400).send(error.message);
+    res.status(400).send(error.message)
   }
-});
+})
 
-app.post("/signup", SignUp);
+app.post('/signup', SignUp)
 
-app.post("/signin", SignIn);
+app.post('/signin', SignIn)
 
-app.use("/products", productController);
+app.use('/products', productController)
 
-app.use("/favourite", favouriteController);
+app.use('/favourite', favouriteController)
 
-app.use("/cart", cartController);
+app.use('/cart', cartController)
 
-app.use("/amount", amountController);
+app.use('/amount', amountController)
 
-app.use("/coupon", couponController);
+app.use('/coupon', couponController)
 
-app.use("/address", addressController);
+app.use('/address', addressController)
 
-app.use("/orders", orderController);
+app.use('/orders', orderController)
 
-module.exports = app;
+module.exports = app
